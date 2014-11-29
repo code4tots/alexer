472cac0e08ef0723388d3880cf09c2e481e5938c
Kyumin Kim <math4tots@users.noreply.github.com>
2014-11-29 11:14:54 -0800

    private member variables

--- alexer/__init__.py
+++ alexer/__init__.py
@@ -16,7 +16,7 @@ class Token(object):
 
 class Lexer(object):
     def __init__(self, symbols, keywords):
-        self.regex_token_pairs = tuple(
+        self._regex_token_pairs = tuple(
             (type_, rec(regex)) for type_, regex in
                 tuple(('KEYWORD', k + r'(?!\w)') for k in keywords) +
                 tuple(('SYMBOL', esc(symbol)) for symbol in symbols) +
@@ -26,15 +26,15 @@ class Lexer(object):
                       ('FLOAT',   r'\d+\.\d*|\.\d+'),
                       ('INT',     r'\d+'),
                       ('NAME',    r'\w+')))
-        self.space_re = rec(r'[ \t]*(?:\#[^\n]*)?')
-        self.empty_lines_re = rec(r'(?:(?:[ \t]*(?:\#[^\n]*)?)(?:\n|\Z))*')
-        self.err_re = rec(r'[^\s]*')
+        self._space_re = rec(r'[ \t]*(?:\#[^\n]*)?')
+        self._empty_lines_re = rec(r'(?:(?:[ \t]*(?:\#[^\n]*)?)(?:\n|\Z))*')
+        self._err_re = rec(r'[^\s]*')
 
     def lex(self, string):
         s = string
-        p = self.regex_token_pairs
-        c = self.space_re
-        e = self.empty_lines_re
+        p = self._regex_token_pairs
+        c = self._space_re
+        e = self._empty_lines_re
         i = e.match(s).end()
         d = ['']
         while i < len(s):
@@ -60,7 +60,7 @@ class Lexer(object):
                         i = m.end()
                         break
                 else:
-                    t = self.err_re.match(s,i).group()
+                    t = self._err_re.match(s,i).group()
                     raise SyntaxError('unrecognized token %r' % (t,))
                 i = c.match(s,i).end()
             yield Token('NEWLINE', s, i)
@@ -78,7 +78,7 @@ class PlyLexer(object):
 
     def __init__(self, symbols_table, keywords):
         self.tokens = (self.tokens +
-                       tuple(symbols_table.values()) + 
+                       tuple(symbols_table.values()) +
                        tuple(k.upper() for k in keywords))
         self._symbols_table = symbols_table
         self._lexer = Lexer(symbols_table.keys(), keywords)
