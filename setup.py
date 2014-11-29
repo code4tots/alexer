794c2df8f024e33020c6d6e5d7db9dd98b146c15
Kyumin Kim <math4tots@users.noreply.github.com>
2014-11-28 19:55:35 -0800

    pypi

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'alexer',
     packages = ['alexer'],
-    version = '0.1',
+    version = '0.2',
     description='dead simple lexer',
     author = 'Kyumin Kim',
     author_email = 'math4tots@gmail.com',
     url = 'https://github.com/math4tots/alexer',
-    download_url = 'https://github.com/math4tots/alexer/0.1',
+    download_url = 'https://github.com/math4tots/alexer/0.2',
     keywords = ['lexer'],
     classifiers = [])
 
